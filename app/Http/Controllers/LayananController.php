0006100293d4b27b0c0451dc8a622671d09c0880
Rfirman30 <20410100059@dinamika.ac.id>
2023-03-03 11:16:05 +0700

    add controller

---
+++ app/Http/Controllers/LayananController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Layanan;
+use Illuminate\Http\Request;
+
+class LayananController extends Controller
+{
+    public function index()
+    {
+        $layanans = Layanan::latest()->paginate(5);
+
+        return view('layanan.index', compact('layanans'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.layanan.create');
+    }
+}
--- resources/views/admin/kebijakan_admin/index.blade.php
+++ resources/views/admin/kebijakan_admin/index.blade.php
@@ -6,30 +6,31 @@
 		<img class="animation__shake" src="assets/img/favicon.png" alt="AdminLTELogo" height="60" width="60">
 	</div>
 	<br>
-	<button type="button" class="btn btn-default" data-toggle="modal" data-target="#modal-lg">Launch Large Modal</button>
-	<div class="card">
 		<div class="card-header">
 			<h3 class="card-title">DataTable with default features</h3>
 		</div>
 		<!-- /.card-header -->
 		<div class="card-body">
+			<a href="{{ route('kebijakans.create') }}" class="btn btn-md btn-success mb-3">TAMBAH KEBIJAKAN</a>	<div class="card">
 			<table id="example1" class="table table-bordered table-striped">
 			<thead>
 			<tr>
-				<th>Rendering engine</th>
-				<th>Browser</th>
-				<th>Platform(s)</th>
-				<th>Engine version</th>
-				<th>CSS grade</th>
-			</tr>
+				<th>NAMA</th>
+				<th>FILE</th>
+				<th>AKSI</th>
+				</tr>
 			</thead>
 			<tbody>
+				@forelse ($kebijakans as $kebijakan)
+					
+				@empty
+					
+				@endforelse
 			<tr>
 				<td>Trident</td>
 				<td>Internet Explorer 4.0</td>
 				<td>Win 95+</td>
 				<td>4</td>
-				<td>X</td>
 			</tr>
 			<tr>
 				<td>Trident</td>
@@ -426,15 +427,6 @@
 				<td>U</td>
 			</tr>
 			</tbody>
-			<tfoot>
-			<tr>
-				<th>Rendering engine</th>
-				<th>Browser</th>
-				<th>Platform(s)</th>
-				<th>Engine version</th>
-				<th>CSS grade</th>
-			</tr>
-			</tfoot>
 			</table>
 		</div>
 		<!-- /.card-body -->
@@ -447,4 +439,23 @@
 </div>
 <!-- /.container-fluid -->
 </div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+<script>
+        //message with toastr
+        @if(session()->has('success'))
+        
+            toastr.success('{{ session('success') }}', 'BERHASIL!'); 
+
+        @elseif(session()->has('error'))
+
+            toastr.error('{{ session('error') }}', 'GAGAL!'); 
+            
+        @endif
+</script>
+
+
 @endsection
