5a8f32d5a1ed5b3973e20d19546d1cc73e855cb9
Rfirman30 <20410100059@dinamika.ac.id>
2023-03-06 14:10:30 +0700

    add

--- app/Http/Controllers/NavbarController.php
+++ app/Http/Controllers/NavbarController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class NavbarController extends Controller
 {
-    //
+    public function index()
+    {
+        $ar_navbar = DB::table('layanans')->get();
+
+        return view('admin.navbarcontrol.index', compact('ar_navbar'));
+    }
 }
