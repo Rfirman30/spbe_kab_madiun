2024c4feafb3f3bb1504c8e7f737b66aab666af8
Rfirman30 <20410100059@dinamika.ac.id>
2023-03-06 08:58:39 +0700

    add

--- resources/views/admin/berita/index.blade.php
+++ resources/views/admin/berita/index.blade.php
@@ -6,30 +6,31 @@
 		<img class="animation__shake" src="assets/img/favicon.png" alt="AdminLTELogo" height="60" width="60">
 	</div>
 	<br>
-	<button type="button" class="btn btn-default" data-toggle="modal" data-target="#modal-lg">Launch Large Modal</button>
-	<div class="card">
 		<div class="card-header">
 			<h3 class="card-title">DataTable with default features</h3>
 		</div>
 		<!-- /.card-header -->
 		<div class="card-body">
+			<a href="{{ route('kebijakans.create') }}" class="btn btn-md btn-success mb-3">TAMBAH KEBIJAKAN</a>	<div class="card">
 			<table id="example1" class="table table-bordered table-striped">
 			<thead>
 			<tr>
-				<th>Rendering engine</th>
-				<th>Browser</th>
-				<th>Platform(s)</th>
-				<th>Engine version</th>
-				<th>CSS grade</th>
-			</tr>
+				<th>NAMA</th>
+				<th>FILE</th>
+				<th>AKSI</th>
+				</tr>
 			</thead>
 			<tbody>
+				@forelse ($kebijakans as $kebijakan)
+					
+				@empty
+					
+				@endforelse
 			<tr>
 				<td>Trident</td>
 				<td>Internet Explorer 4.0</td>
 				<td>Win 95+</td>
 				<td>4</td>
-				<td>X</td>
 			</tr>
 			<tr>
 				<td>Trident</td>
@@ -248,193 +249,7 @@
 				<td>420.1</td>
 				<td>A</td>
 			</tr>
-			<tr>
-				<td>Webkit</td>
-				<td>S60</td>
-				<td>S60</td>
-				<td>413</td>
-				<td>A</td>
-			</tr>
-			<tr>
-				<td>Presto</td>
-				<td>Opera 7.0</td>
-				<td>Win 95+ / OSX.1+</td>
-				<td>-</td>
-				<td>A</td>
-			</tr>
-			<tr>
-				<td>Presto</td>
-				<td>Opera 7.5</td>
-				<td>Win 95+ / OSX.2+</td>
-				<td>-</td>
-				<td>A</td>
-			</tr>
-			<tr>
-				<td>Presto</td>
-				<td>Opera 8.0</td>
-				<td>Win 95+ / OSX.2+</td>
-				<td>-</td>
-				<td>A</td>
-			</tr>
-			<tr>
-				<td>Presto</td>
-				<td>Opera 8.5</td>
-				<td>Win 95+ / OSX.2+</td>
-				<td>-</td>
-				<td>A</td>
-			</tr>
-			<tr>
-				<td>Presto</td>
-				<td>Opera 9.0</td>
-				<td>Win 95+ / OSX.3+</td>
-				<td>-</td>
-				<td>A</td>
-			</tr>
-			<tr>
-				<td>Presto</td>
-				<td>Opera 9.2</td>
-				<td>Win 88+ / OSX.3+</td>
-				<td>-</td>
-				<td>A</td>
-			</tr>
-			<tr>
-				<td>Presto</td>
-				<td>Opera 9.5</td>
-				<td>Win 88+ / OSX.3+</td>
-				<td>-</td>
-				<td>A</td>
-			</tr>
-			<tr>
-				<td>Presto</td>
-				<td>Opera for Wii</td>
-				<td>Wii</td>
-				<td>-</td>
-				<td>A</td>
-			</tr>
-			<tr>
-				<td>Presto</td>
-				<td>Nokia N800</td>
-				<td>N800</td>
-				<td>-</td>
-				<td>A</td>
-			</tr>
-			<tr>
-				<td>Presto</td>
-				<td>Nintendo DS browser</td>
-				<td>Nintendo DS</td>
-				<td>8.5</td>
-				<td>
-					C/A<sup>1</sup>
-				</td>
-			</tr>
-			<tr>
-				<td>KHTML</td>
-				<td>Konqureror 3.1</td>
-				<td>KDE 3.1</td>
-				<td>3.1</td>
-				<td>C</td>
-			</tr>
-			<tr>
-				<td>KHTML</td>
-				<td>Konqureror 3.3</td>
-				<td>KDE 3.3</td>
-				<td>3.3</td>
-				<td>A</td>
-			</tr>
-			<tr>
-				<td>KHTML</td>
-				<td>Konqureror 3.5</td>
-				<td>KDE 3.5</td>
-				<td>3.5</td>
-				<td>A</td>
-			</tr>
-			<tr>
-				<td>Tasman</td>
-				<td>Internet Explorer 4.5</td>
-				<td>Mac OS 8-9</td>
-				<td>-</td>
-				<td>X</td>
-			</tr>
-			<tr>
-				<td>Tasman</td>
-				<td>Internet Explorer 5.1</td>
-				<td>Mac OS 7.6-9</td>
-				<td>1</td>
-				<td>C</td>
-			</tr>
-			<tr>
-				<td>Tasman</td>
-				<td>Internet Explorer 5.2</td>
-				<td>Mac OS 8-X</td>
-				<td>1</td>
-				<td>C</td>
-			</tr>
-			<tr>
-				<td>Misc</td>
-				<td>NetFront 3.1</td>
-				<td>Embedded devices</td>
-				<td>-</td>
-				<td>C</td>
-			</tr>
-			<tr>
-				<td>Misc</td>
-				<td>NetFront 3.4</td>
-				<td>Embedded devices</td>
-				<td>-</td>
-				<td>A</td>
-			</tr>
-			<tr>
-				<td>Misc</td>
-				<td>Dillo 0.8</td>
-				<td>Embedded devices</td>
-				<td>-</td>
-				<td>X</td>
-			</tr>
-			<tr>
-				<td>Misc</td>
-				<td>Links</td>
-				<td>Text only</td>
-				<td>-</td>
-				<td>X</td>
-			</tr>
-			<tr>
-				<td>Misc</td>
-				<td>Lynx</td>
-				<td>Text only</td>
-				<td>-</td>
-				<td>X</td>
-			</tr>
-			<tr>
-				<td>Misc</td>
-				<td>IE Mobile</td>
-				<td>Windows Mobile 6</td>
-				<td>-</td>
-				<td>C</td>
-			</tr>
-			<tr>
-				<td>Misc</td>
-				<td>PSP browser</td>
-				<td>PSP</td>
-				<td>-</td>
-				<td>C</td>
-			</tr>
-			<tr>
-				<td>Other browsers</td>
-				<td>All others</td>
-				<td>-</td>
-				<td>-</td>
-				<td>U</td>
-			</tr>
 			</tbody>
-			<tfoot>
-			<tr>
-				<th>Rendering engine</th>
-				<th>Browser</th>
-				<th>Platform(s)</th>
-				<th>Engine version</th>
-				<th>CSS grade</th>
-			</tr>
-			</tfoot>
 			</table>
 		</div>
 		<!-- /.card-body -->
@@ -447,4 +262,23 @@
 </div>
 <!-- /.container-fluid -->
 </div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+<script>
+        //message with toastr
+        @if(session()->has('success'))
+        
+            toastr.success('{{ session('success') }}', 'BERHASIL!'); 
+
+        @elseif(session()->has('error'))
+
+            toastr.error('{{ session('error') }}', 'GAGAL!'); 
+            
+        @endif
+</script>
+
+
 @endsection
