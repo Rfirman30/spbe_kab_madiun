5c930433968ebe13f5a1332b8f125b305e45160c
Rfirman30 <20410100059@dinamika.ac.id>
2023-03-08 09:52:52 +0700

    add

--- app/Http/Controllers/TataKelolaController.php
+++ app/Http/Controllers/TataKelolaController.php
@@ -2,15 +2,68 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB as TataDB;
 
-class TataKelolaController extends Controller
+class TatakelolaController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
         $ar_tata = TataDB::table('tatakelolas')->get();
 
         return view('admin.tatakelola.index', compact('ar_tata'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
