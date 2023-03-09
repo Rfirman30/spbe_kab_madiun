de97af23ef35dc9ec2cd04ab97e5bff516143d80
Rfirman30 <20410100059@dinamika.ac.id>
2023-03-09 14:13:37 +0700

    add

--- database/migrations/2023_03_09_045132_create_manajemens_table.php
+++ database/migrations/2023_03_09_045132_create_manajemens_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('manajemens', function (Blueprint $table) {
             $table->id();
+            $table->string('nama');
+            $table->string('file');
             $table->timestamps();
         });
     }
